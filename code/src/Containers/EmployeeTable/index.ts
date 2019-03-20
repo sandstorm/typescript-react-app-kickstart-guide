2573a488feb41373a90c998067f2bf384d03deab
Robert Baruck <robert.baruck@sandstorm-media.de>
2019-03-20 10:57:21 +0100

    Deploy website
    Deploy website version based on cddd8fbf353f7f3e6e5c400d178eefef241cc0b2

--- code/src/Containers/EmployeeTable/index.ts
+++
@@ -1,2 +0,0 @@
-import EmployeeTable from './EmployeeTable';
-export default EmployeeTable;
--- code/src/Domain/Company.ts
+++
@@ -1,6 +0,0 @@
-export interface Company {
-  readonly country: string;
-  readonly id: string;
-  readonly name: string;
-  readonly phone: string;
-}
